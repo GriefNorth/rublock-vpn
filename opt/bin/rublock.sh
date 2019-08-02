7c62c83f23149710da4ca5ea62c522ae32214e01
blackcofee <42971175+blackcofee@users.noreply.github.com>
2019-08-02 18:48:59 +0900

    Update rublock.sh

--- opt/bin/rublock.sh
+++ opt/bin/rublock.sh
@@ -1,13 +1,18 @@
 #!/bin/sh
 
-echo List Generation
+echo List generation
 /opt/bin/blupdate.lua
 
-echo Clear List
+echo Clear the list
 cd /opt/etc
 sed -i '/pornhub.com/d' rublock.dnsmasq
-sed -i '/youtube.com/d' rublock.dnsmasq
-sed -i '/googleusercontent.com/d' rublock.dnsmasq
+sed -i '/youtube.com/d; /googleusercontent.com/d' rublock.dnsmasq
+sed -i '/vulkan/d; /wulkan/d; /vulcan/d' rublock.dnsmasq
+sed -i '/casino/d; /kasino/d; /azino/d' rublock.dnsmasq
+sed -i '/1xbet/d; /betcity/d; /leon/d; /fonbet/d' rublock.dnsmasq
+sed -i '/stavka/d; /slot/d; /dosug/d' rublock.dnsmasq
+
+echo Add custom sites
 sed -i '$aipset=\/nnm-club.ws\/rublock' rublock.dnsmasq
 sed -i '$aipset=\/gnome-look.org\/rublock' rublock.dnsmasq
 sed -i '$aipset=\/opendesktop.org\/rublock' rublock.dnsmasq
@@ -16,6 +21,8 @@ sed -i '$aipset=\/7-zip.org\/rublock' rublock.dnsmasq
 sed -i '$aipset=\/reactos.org\/rublock' rublock.dnsmasq
 sed -i '$aipset=\/nextcloud.com\/rublock' rublock.dnsmasq
 sed -i '$aipset=\/git.openwrt.org\/rublock' rublock.dnsmasq
+
+echo Add custom ip
 sed -i '$a91.108.4.0\/22' rublock.ips
 sed -i '$a91.108.8.0\/22' rublock.ips
 sed -i '$a91.108.12.0\/22' rublock.ips
@@ -26,7 +33,7 @@ sed -i '$a149.154.164.0\/22' rublock.ips
 sed -i '$a149.154.168.0\/22' rublock.ips
 sed -i '$a149.154.172.0\/22' rublock.ips
 
-echo Add ip
+echo Add ip to table
 ipset flush rublock
 
 for IP in $(cat /opt/etc/rublock.ips) ; do
